db6dc2f66adcc59c1186e607e748381fe7e74c8f
Vishal-Antiya <vishalantiya1111@gmail.com>
2024-07-19 04:58:21 +0000

    variables

---
+++ basics/variables.js
@@ -0,0 +1,16 @@
+const accId = 15 //const declares a constant variale
+let accName = "vishal"// let decares a variable that can be changed
+var accpPass = "12345"//var is an old version of let DO NOT USE IT
+accCity = "Mumbai"// in javacript you can also declare a variable without anything, but NOT PREFERABLE
+
+// accID = 20 // NOT ALLOWED
+
+console.log(accId);
+
+console.table([accId, accName, accpPass, accCity]);
+
+accName = "bhavya"
+accpPass = "54321"
+accCity = "pune"
+
+console.table([accId, accName, accpPass, accCity]);
