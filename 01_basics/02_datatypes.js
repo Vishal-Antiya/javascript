6dd5339ef78a5458fdeda7db8ebfd31c561fc3f1
Vishal-Antiya <vishalantiya1111@gmail.com>
2024-07-19 05:25:20 +0000

    data typesss

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict" //this will makw the whole code use newer versions of js
+
+// for documentation use: "mdn javascript docs" & "tc39.es"
+
+let name = "vishal"
+let age = 20
+let married = false
+
+/*
+number = limit is 2 to the power of 53
+bigint 
+string
+boolean: true / flase
+null: standaolone value / empty value
+undefined: when you sont assign a value to a variable.
+*/
+
+//console.log(typeof XXXX); //tells you the datattype of the value you put in XXXX
+
+console.log(typeof "vishal"); //string
+console.log(typeof age); //number
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
