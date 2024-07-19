8d94cd88ca4a134fc169c292e95623c3834b6d3b
Vishal-Antiya <vishalantiya1111@gmail.com>
2024-07-19 06:12:42 +0000

    conversion of data types & operatoions

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -30,3 +30,31 @@ previous value => converted value
 null => false
 undefined => false
 */
+
+/***************OPERTIONS************************/
+
+let val = 3;
+negval = -val
+console.log(negval); //-3 
+
+// console.log(2 + 2);  ADD
+// console.log(2 - 2);  SUBTRACT
+// console.log(2 * 2);  MULTIPLY
+// console.log(2 / 2);  DIVIDE
+// console.log(2 % 2);  REMAINDER
+// console.log(2 ** 5); 2 TO THE POWER OF 5
+
+
+let str1 = "hello";
+let str2 = " vishal";
+
+let str3 = str1 + str2; // adds 2 strings together
+console.log(str3);
+
+console.log("1" + 2);     //op = 12
+console.log(1 + "2");     //op = 12
+console.log("1" + 2 + 2); //op = 122
+console.log(1 + 2 + "2"); //op = 32
+
+console.log( +true);   //op = 1. converts true to number.
+// console.log(true+); //error. cannot compute
