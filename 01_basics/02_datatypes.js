1a1544721718e2f88f9eacec4882cee0d2d21dc6
Vishal-Antiya <vishalantiya1111@gmail.com>
2024-07-20 07:12:16 +0000

    data-types and their memory allocation in js

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -44,3 +44,45 @@ let myObj = {                                    // this is an object. VERY IMP
 const myFunc = function(){                       // A FUNCTION.
     console.log("hello world");
 }
+
+/*________________________________________________________________________________________________________________________________
+
+===============================================================
+    In JS the variables are either stored in STACK or in HEAP. 
+    All the PRIMITIVE data types are stored in STACK. 
+    While the NON-PRIMITIVE are stored in HEAP.
+================================================================
+*/
+
+let name1 = "vishal"; // string, a primitive data type
+
+let name2 = name1;   //  here name2 gets the value 'vishal'
+
+name2 = "bhavya"     //  but here the name2 gets another value 'bhavya'
+
+console.log(name1);  // op: vishal
+console.log(name2);  // op: bhavya
+
+/* When stack gets another value(name2) assigned to first(name1) it creates a duplicate of first(name1).
+So when you change name2's value to bhavya name1 remains the same, because only the duplicate has been changed.*/
+
+
+let user1 = {                       // this is an object. a non-primitive data-type 
+    email: "vishal@google.com",
+    upi: "vishal@ybl"
+}
+
+let user2 = user1;  
+
+user2.email = "vishal@facebook.com"
+
+console.log(user1.email);   // op = vishal@facebook.com
+console.log(user2.email);   // op = vishal@facebook.com
+
+/* For a non-primitive data type, JS will store the value in a heap.
+    So  {email: "vishal@google.com",
+         upi: "vishal@ybl"}   will be stored in heap.
+    At the same time variable called 'user1' will be created in stack and be assigned this value.
+    
+ -> Now when you declare user2, it will be created in stack and assigned the same value as user1 in the heap.
+ -> So when you change user2's email, user1's email changes along with it, because they are both assigned to the same value in heap.*/
