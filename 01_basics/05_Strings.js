3bd3fc621e5ddd81bf26f84a69f07781971ad612
Vishal-Antiya <vishalantiya1111@gmail.com>
2024-07-20 09:32:28 +0000

    Strings in js

--- 01_basics/05_Strings.js
+++ 01_basics/05_Strings.js
@@ -5,7 +5,7 @@ const repoCount = 50
 
 console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 
-const gameName = new String('vishal-antiya')
+const gameName = new String('vishal-antiya') // This is a STRING OBJECT, created using the 'String()' function
 
 console.log(gameName[0]);
 console.log(gameName.__proto__);
