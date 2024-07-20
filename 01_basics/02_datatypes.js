159caf642d6e8a0195b2ebdfdc5f140487cbef85
Vishal-Antiya <vishalantiya1111@gmail.com>
2024-07-20 06:36:26 +0000

    data types in js

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -1,23 +1,46 @@
-"use strict" //this will makw the whole code use newer versions of js
+"use strict" //this will make the whole code use newer versions of js
 
-// for documentation use: "mdn javascript docs" & "tc39.es"
+// for documentation use: "mdn javascript docs"  &  "tc39.es"
+
+/***************************PRIMITIVE DATATYPES*************************/
+
+/*
+number  (limit is 2 to the power of 53)
+bigint 
+string
+boolean: true / flase
+null: standaolone value / empty value
+undefined: when you dont assign a value to a variable.
+*/
 
 let name = "vishal"
 let age = 20
 let married = false
 
-/*
-number = limit is 2 to the power of 53
-bigint 
-string
-boolean: true / flase
-null: standaolone value / empty value
-undefined: when you sont assign a value to a variable.
-*/
-
 //console.log(typeof XXXX); //tells you the datattype of the value you put in XXXX
 
-console.log(typeof "vishal"); //string
-console.log(typeof age); //number
-console.log(typeof null); //object
-console.log(typeof undefined); //undefined
+console.log(typeof "vishal");   //string
+console.log(typeof age);        //number
+console.log(typeof null);       //object
+console.log(typeof undefined);  //undefined
+
+
+/****************************NON-PRIMITIVE DATATYPES*********************************/
+
+/*
+arrays,
+objects,
+functions
+*/
+
+const heroes = ["ironman", "spiderman", "batman"] //this is how you declare an array
+
+let myObj = {                                    // this is an object. VERY IMP
+    name: "Vishal",
+    age: 20,
+    job: "student",
+}
+
+const myFunc = function(){                       // A FUNCTION.
+    console.log("hello world");
+}
